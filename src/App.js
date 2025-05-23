9d75632be2b945eb2b6a1845077dd03092f796d2
abhisheksain841 <abhisheksain841@gmail.com>
2025-05-23 15:36:44 +0530

    gfhgf

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -14,16 +10,16 @@ function App() {
   return (
     <>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/product" element={<Product />}></Route>
-          <Route path="*" element={<h2>Page Not Found*****</h2>}></Route>
+          <Route path="*" element={<h2>Page Not Found***</h2>}></Route>
         </Routes>
-        <Footer/>
-      </Router> 
+        <Footer />
+      </Router>
     </>
   );
 }
